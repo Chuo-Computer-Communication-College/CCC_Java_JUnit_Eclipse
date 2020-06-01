8a4172601ea550fbf3ca60b3ead50135f04fe7a4
Hiro281 <44799467+Hiro281@users.noreply.github.com>
2020-06-01 15:40:54 +0900

    List 1.6

--- 978-4-7741-5377-3/junit-tutorial/test/junit/tutorial/CalculatorTest.java
+++ 978-4-7741-5377-3/junit-tutorial/test/junit/tutorial/CalculatorTest.java
@@ -8,13 +8,25 @@ import org.junit.Test;
 public class CalculatorTest
 {
     @Test
-    public void multiplyで乗算結果が取得できる()
+    public void multiplyで３と４の乗算結果が取得できる()
     {
         Calculator cltr = new Calculator();
 
         int intExpected = 12;
 
         int intActual = cltr.multiply(3, 4);
+
+        assertThat(intActual, is(intExpected));
+    }
+
+    @Test
+    public void multiplyで５と７の乗算結果が取得できる()
+    {
+        Calculator cltr = new Calculator();
+
+        int intExpected = 35;
+
+        int intActual = cltr.multiply(5, 7);
 
         assertThat(intActual, is(intExpected));
     }
